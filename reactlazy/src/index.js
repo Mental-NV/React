e3d48bb0cde926075d23822529b1c018982cc38a
Ruslan Galiev (Akvelon INC) <v-rugali@microsoft.com>
2020-09-06 20:11:48 +0300

    Added react routing

--- reactlazy/src/index.js
+++ reactlazy/src/index.js
@@ -1,19 +1,24 @@
 
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css'
 
 const Home = React.lazy(() => import('./home'));
 const About = React.lazy(() => import('./about'));
+const Users = React.lazy(() => import('./users'));
 
 function App(props) {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <section>
-                <Home />
-                <About />
-            </section>
-        </Suspense>
+        <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/users" component={Users} />
+                </Switch>
+            </Suspense>
+        </Router>
     );
 }
 
