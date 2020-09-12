28cc0d6116368fa84690d8929e830a51bc7d6af1
Ruslan Galiev (Akvelon INC) <v-rugali@microsoft.com>
2020-09-12 23:56:51 +0300

    Logo and app bar UI design

--- notesbin/src/components/navbar.js
+++ notesbin/src/components/navbar.js
@@ -6,34 +6,29 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { styled } from '@material-ui/core/styles';
-
-const NavLink = styled(Link)({
-    color: 'white',
-    marginLeft: '10px',
-});
-
-const StyledAppBar = styled(AppBar)({
-    marginBottom: '15px',
-});
+import './navbar.css';
 
 const NavBar = () => {
     return (
-        <StyledAppBar position="static">
+        <AppBar className='nav' position="static">
             <Toolbar>
-                <Typography variat="title" color="inherit">
-                    Notesbin
-                </Typography>
-                <Typography>
-                    <NavLink component={RouterLink} to="/" >
-                        Notes list
-                    </NavLink>
-                    <NavLink component={RouterLink} to="/create" >
-                        Create notes
-                    </NavLink>
-                </Typography>
+                <div className='logo'>
+                    <Typography>
+                        <Link className='logolink navlink' component={RouterLink} to="/" >
+                            <img src='logo32.png' alt='Notesbin' title='Notesbin' />
+                            <span>Notesbin</span>
+                        </Link>
+                    </Typography>
+                </div>
+                <div className='buttons'>
+                    <Typography>
+                        <Link className='navlink' component={RouterLink} to="/create" >
+                            Create notes
+                        </Link>
+                    </Typography>
+                </div>
             </Toolbar>
-        </StyledAppBar>
+        </AppBar>
     );
 }
 
